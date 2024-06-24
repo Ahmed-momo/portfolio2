97612f767be6c196747386b2f3d3aa787e46eea7
Ahmed-momo <am3404752@gmail.com>
2024-06-24 15:34:00 +0300

    Add files via upload

---
+++ ahmedjj.js
@@ -0,0 +1,468 @@
+//var name ="ahmed"
+//console.log(name.length)
+//document.write(name.repeat(1))
+//let name2 = "mohmed"
+//console.log(name2[5])
+//let name3 = "mohmed"
+//console.log(name3.charAt(1))
+//let name4 = "i love ahmed"
+//console.log(name4.indexOf("ahmed"))
+//let name5 = "my name is ahmed"
+//console.log(name5.lastIndexOf("i"))
+//let name6 = "is very good?"
+//console.log(name6.slice(2))
+//let name7 = "GOMANNA AHMED"
+//console.log(name7.split(" "))
+//! let name8 = "GOMANNA AHMED"
+// todo let name8 = "GOMANNA AHMED"
+//document.write(name8.split("  "))//
+//todo let name = 'ahmed mohmed'
+//console.log(name.includes('e'))
+//let name2 = 'ahmed mohmed'
+//console.log(name2.startsWith('ahmed'))
+//let name3 = 'ahmed mohmed'
+//console.log(name3.endsWith('d'))
+//!let objects = [1,2,3,[4,5,6,[7,8,9]]]
+//!console.log(objects[3][3][2]) 
+//!var names = ['ahmed','mohmed','ali','omer','kareem','gomanna']
+//!names[5] = 'rania'
+//!names[0] = 'amal'
+//!names[2] = 'abdelrahman'
+//!console.log(names)
+
+// todo let names = ['ahmed', 'mohmed', 'omer', 'ali']     //!push بضيف عنصر في الاخر
+// todonames.push('kiri','corona')
+// todoconsole.log(names)
+// todo let names1 = ['ahmed', 'mohmed', 'omer', 'ali']     //todo unshift بضيف عنصر في الاول
+// todonames1.unshift('kiri','corona')
+// todoconsole.log(names1)
+// let names2 = ['ahmed', 'mohmed', 'omer', 'ali']
+// names2.shift('')
+// console.log(names2) 
+
+
+
+// todolet names3 = ['ahmed', 'mohmed', 'omer', 'ali']
+// todonames3.pop()
+// todonames3.pop()
+// todoconsole.log(names3)
+
+// todovar names7 = ['yara','lara', 'samah' ,'alaa']
+// todonames7.splice(1,1,'ahmed','mohmed')
+// todoconsole.log(names7)
+// todolet names8 = ['mona','rania','zeinab']
+
+// todoconsole.log(names8.slice(-2,0))
+// todoconsole.log(names8)
+
+
+//var arr = ['ahmed','kamal','omer','zli','mark']
+//console.log(arr.reverse())
+//var arr = ['ahmed','kamal','omer','zli','mark']
+//console.log(arr.sort())
+//var arr = [9,0,4,1,2]
+//console.log(arr.sort())
+ 
+//!var arr1 = ['ahmed','mohmed','omer','kareem']
+//!var arr2 = ['mona','rania','amal']
+//!var arr3 = ['ahmed','mohmed','omer','kareem']
+//!console.log(arr1.concat(arr2,arr3,'vironica'))
+
+//!let arr4 = ['ali','karma','angila white']
+//!console.log(arr4.join('-'))
+ //todo let askemail = prompt('enter your email please');
+ //todo let email = 'ahmed mohmed.com';
+ //todo console.log(askemail.toLocaleLowerCase().trim() == email)
+
+
+
+
+
+//var ahmed = prompt('enter your email')
+//var email2 = 'ahmed mohmed';
+//console.log(ahmed .toLocaleLowerCase().trim()== email2)
+
+
+
+
+
+
+
+
+
+
+
+//todo let z = 'ahmed';
+//todo console.log(z.toLocaleUpperCase())
+//todo let a = 'ahmed';
+//todo console.log(a.toLowerCase())
+
+
+
+
+
+
+
+
+//let next = document.querySelector('.prev') 
+//let prev = document.querySelector('.next')
+
+//next.addEventListener('click', function(){
+ //   let items = document.querySelectorAll('.item')
+ //   document.querySelector('.slide').appendChild(items[0])
+//})
+
+//prev.addEventListener('click', function(){
+//    let items = document.querySelectorAll('.item')
+//    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+//})
+
+
+//! && = and
+//! || = or 
+//! == valus 
+//! === valus and datetype  نوع الحاجه
+// todo console.log(4 <= 4)
+// todo console.log(10 != 10)
+
+// todo let product = 'Iphone';
+// todo let price = 1000;
+// todo let size = 10;
+// todo console.log(price < 10 || size ==50   )
+
+// todo let kind = 'male';
+// todo let age = 18;
+// todo let name = 'ahmed';
+// todo console.log(age <=20 && name =='ahmed' || kind == 'female')
+
+
+//!let num1  =  prompt('num1')
+//!let num2  = prompt('num2')
+
+//!if(num2 > num1)
+//!{
+    //!console.log('can you kill me')
+//!}
+
+
+// todo let role = prompt('what is your role')
+// todo if(role =='admain')
+// todo {
+ // todo    document.write('create  delay  fucking')
+
+// todo }
+// todo else if(role == 'manager')
+// todo     {
+// todo         document.write('caling  rania')
+// todo     }
+// todo else if(role == 'kiling')                                      < if > , < else >
+// todo     {
+// todo         document.write('kill')
+// todo     }  
+
+// todo else{
+ // todo    document.write('fuck  you')
+// todo }
+
+
+
+//! name = prompt('what is your name?');
+//! name =='ahmed'?
+//! document.write('hello ahmed')
+//! :name == 'mohmed'?
+//! document.write('hello mohmed')
+//! :document.write('Koria');
+
+
+
+
+
+// todo let age = prompt('what is your age');
+// todo let result = age > 18 ?
+// todo     'hello user' :
+// todo     age == 18 ?
+// todo         'you are 18 now' :
+// todo         'you are under 18';
+
+// todo console.log(result);
+
+//let  role = prompt('what is your role');
+//switch(role)
+//{
+//    case 'admain':
+//    document.write('create ,update contraling');
+//    break;                                                         <<switch>>
+//    case 'manegar':
+//        document.write('poistion');
+//        break;
+//        case 'ahmed':
+//        document.write('can  sahra arabic');
+//        break;
+//        default:
+//            document.write('can i kill you?')
+//
+//}
+
+
+//!  var kor = prompt('what is your kor?');
+//!  switch(kor)
+//!  {
+//!      case 'kareem':
+//!          document.write('thaks for your choise')
+//!          break;                                              <<switch>>
+//!          case 'rania':
+//!              document.write('thaks for your choice')
+//!              break;
+//!              case 'zinab':
+              //!  document.write('some people have inforation')
+      //!          break;
+//!  }
+
+//?  let hr = prompt('what is your name?');
+//?  switch(hr)
+//?  {                                                         
+//?      case 'ahmed':
+//?         console.log('hello ahmed')                      switch
+//?         break;
+//?          default:
+//?             console.log('hello user');
+//? }
+
+
+//!<<start    ;    condition =(end)  ;     steps>>
+
+// ? for(let e = 0; e <=6; e+=2)
+// ? {
+// ? console.log('ahmed' + e)
+// ? }
+
+
+// ? for(let p = 0 ; p <=10; p+=2)
+// ?     {
+ // ?        console.log('omer' +p)
+ // ?    }
+
+// ? for(let k = 10; k >=  0; k-=2 )
+ // ?    {
+       //? console.log( k);
+ // ?    }
+    
+
+
+// ? for( let L=0 ; L <=10;   L++ )
+ // ?    {
+ // ?        console.log('GOMANNA' + L)
+ // ?    }
+
+// ? for( let G=10 ; G >=0 ;   G-- )
+ // ?    {
+ // ?        console.log('goma' + G)
+ // ?    }
+
+
+
+
+     // ! let namess=[' ahmed', ' mohmed' , ' omer']
+     // ! for(let o = 0 ; o<=4 ; o++){
+      // !      console.log(namess[o])
+
+
+      // !}
+  
+
+// ?var car =['artura','merceder','BMW']
+// ?var model = [2022,2023,2024]
+// ?var colors = ['red','blue','green']
+// ?for( let l = 0;l<car.length; l++ )
+// ?      {
+// ?            console.log(`car:${car[l]}`);
+     // ?       for(let g = 0 ; g <model.length ;g++)               <<<<<<<<( insted loop)>>>>>>>
+      // ?            console.log(`model:${model[g]}`)
+      // ?      for(let f= 0 ;f <colors.length   ; f++ )
+      // ?            {
+      // ?                  console.log(`colors:${colors[f]}`)
+
+       // ?           }
+       // ?     console.log(`______________________`)
+
+
+      // ?}
+
+// todo var clints = ['mohmed','omer','kareem','koriana','madona']
+// todo for( let w = 0; w < clints.length  ; w++ )
+ // todo      {
+// todo             if( clints[w] =='mohmed')
+ // todo                  {
+      // todo                  continue;
+ // todo                  }
+ // todo                  console.log(clints[w])
+
+  // todo     }
+     // todo  var clints = ['mohmed','omer','kareem','koriana','madona']
+// todo for( let w = 0; w < clints.length  ; w++ )
+  // todo     {
+  // todo           if( clints[w] !='mohmed')
+   // todo                {
+    // todo                     continue;
+    // todo               }
+     // todo              console.log(clints[w])
+
+     // todo  }
+     // todo  var clints = ['mohmed','omer','kareem','koriana','madona']
+// todo for( let w = 0; w < clints.length  ; w++ )
+  // todo     {
+    // todo         if( clints[w] =='koriana')
+    // todo               {
+    // todo                     break;
+     // todo              }
+     // todo              console.log(clints[w])
+
+     // todo  }
+
+// ?let omer =['tamara','kamara','omaraia','rania']
+// ?for( let d =0 ; d < omer.length ;d++ )
+// ?      {
+// ?            if(  omer [d] =='omaraia')
+// ?                  {
+ // ?                       break;
+
+ // ?                 }
+            
+ // ?           console.log(omer[d])
+
+  // ?    }
+
+
+
+// todo var k =0;
+// todo while(k<3){
+// !      document.write('<h1>AHMED MOHMED</h1>')                    loop = while
+// todo       k++
+// todo }
+
+// ? var L = 0;
+// ? do{
+  // ?     document.write('<h1>MY NAME IS AMEDMOHMED</h>')
+     // ?  L++
+
+// ? }while(false)
+
+
+
+
+
+
+
+
+
+// todo function Hi(name)
+// todo {
+   // todo    document.write('hi' +name)// todo          ((<<<<<<<<<< function 1>>>>>>>>>>))
+// todo }
+// todo Hi(' ahmed')
+
+
+
+
+
+window.addEventListener("scroll",function(){
+     const header = this.document.querySelector("header")
+     header.classList.toggle("sticky" ,window.scrollY > 0)
+})
+
+
+
+
+
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+
+var modal = function(modalClick){
+     serviceModals[modalClick].classList.add("active");
+
+}
+
+learnmoreBtns.forEach((learnmoreBtns, i) =>{
+     learnmoreBtns.addEventListener("click",() =>{
+          modal(i);
+     })
+})
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+     modalCloseBtns.addEventListener("click",() =>{
+          serviceModals.forEach((modalView) =>{
+               modalView.classList.remove("active");
+
+          });
+     });
+});
+
+
+
+
+
+const themeBtn = document.querySelector(".theme-btn")
+
+themeBtn.addEventListener("click" , () =>{
+     document.body.classList.toggle("dark-theme")
+     themeBtn.classList.toggle("sun")
+
+     localStorage.setItem("saved-theme", getCurrentTheme())
+     localStorage.setItem("saved-icon" , getCurrentIcon())
+})
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
+
+const savedTheme = localStorage.getItem("saved-theme")
+const savedIcon = localStorage.getItem("saved-icon")
+
+if(savedTheme)
+     {
+          document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme")
+          themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun")
+
+     }
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+const portfolioModals = document.querySelectorAll(".porfolio-model");
+const imgcards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+
+var portfolioModal = function(modalClick){
+     portfolioModals[modalClick].classList.add("active");
+
+}
+
+imgcards.forEach((imgcards, i) =>{
+     imgcards.addEventListener("click",() =>{
+          portfolioModal(i);
+     })
+})
+portfolioCloseBtns.forEach((portfolioCloseBtns) => {
+     portfolioCloseBtns.addEventListener("click",() =>{
+          portfolioModals.forEach((portfolioModalView) =>{
+               portfolioModalView.classList.remove("active");
+
+
+
+          });
+     });
+});
